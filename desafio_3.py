9427a9c5cfd00436e0f9749c48f90fd743b88cf3
Henrique Bueno Foseca <108840429+HenriBueno00@users.noreply.github.com>
2023-02-28 20:58:12 -0300

    Desafio 3

---
+++ desafio_3.py
@@ -0,0 +1,10 @@
+lista=[]
+for i in range(3):
+    lista.append(input("Digite um numero: "))
+    
+maior=max(lista)
+menor=min(lista)
+print("Os numeros informados",lista)
+print("O maior numero informado é: ",maior)
+print("O menor numero informado é: ",menor)
+    
